27a0b28d888eee697fbc826f6321cb5f8dab0720
Martin Saldinger <51637671+LeTamanoir@users.noreply.github.com>
2023-03-18 11:40:59 +0100

    [ADD] fix about

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -7,14 +7,9 @@ export default function About() {
       />
 
       <div className="w-full px-5 sm:pl-8 text-base leading-7">
-        My name is <b className="text-lg">Martin Saldinger</b>. I love{" "}
-        <b className="text-lg">computer science</b>, precisely{" "}
-        <b className="text-lg">web development</b>{" "}
-        <b className="text-lg">and pentesting</b>. <br />
-        In my free-time I like to code and practice my hacking skills by solving
-        CTFs. <br />
-        I'm currently studying at <b className="text-lg">Epitech Paris</b> in
-        the International Track program.
+        My name is <b className="text-lg">Martin Saldinger</b>. I am passionate about{" "}
+        <b className="text-lg">computer science</b>.
+        I'm currently studying at <b className="text-lg">Epitech Paris</b>.
       </div>
     </main>
   );
